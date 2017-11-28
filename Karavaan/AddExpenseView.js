a438fe36668762144ab223336e3447f2ef369065
BrechtDecuyper <brechtdecuyper@hotmail.com>
2017-11-28 09:47:43 +0100

    Starting to make second part addExpense

--- Karavaan/AddExpenseView.js
+++ Karavaan/AddExpenseView.js
@@ -1,6 +1,12 @@
 import React from 'react';
 import { Picker, View, TextInput, Text, Button } from 'react-native';
 
+var SPLITMETHOD = {
+    OWN_SHARE    : { name: "Everyone pays his own share"},
+    DIVIDED_EVEN : { name: "Divided evenly"},
+    WAY_OF_BILL  : { name: "By way of a bill"}
+}
+
 export default class AddExpenseView extends React.Component
 {
     static navigationOptions = ({navigation}) => ({
@@ -10,14 +16,19 @@ export default class AddExpenseView extends React.Component
     constructor(props)
     {
         super(props);
-        this.state = { category: '', date: '', currencies: this.props.navigation.state.params.trip.currencies, splittingMethod: ''};
+        this.state = { category: '', date: '', currency: '', splittingMethod: ''};
     }
 
     render()
     {
-        currencyPicker = this.state.currencies.map( c => {
+        currencyPicker = this.props.navigation.state.params.trip.currencies.map( c => {
             return (
                 <Picker.Item label={c} value={c}/>
+        )});
+
+        splitPicker = this.SPLITMETHOD.map( s => {
+            return (
+                <Picker.Item label={s.name} value={s}/>
         )});
 
         return (
@@ -35,21 +46,65 @@ export default class AddExpenseView extends React.Component
                 <Text>Add a date: {this.props.bar}</Text>
                 <TextInput placeholder='DD/MM/YYYY' onChangeText={(text) => this.setState({date: text})}/>
                 <Text>Choose a currency: {this.props.bar}</Text>
-                <Picker>
+                <Picker
+                    selectedValue={this.state.currency}
+                    onValueChange={(itemValue, itemIndex) => this.setState({currency: itemValue})}>
                     {currencyPicker}
                 </Picker>
                 <Text>How to split the bill? {this.props.bar}</Text>
                 <Picker
                     selectedValue={this.state.splittingMethod}
                     onValueChange={(itemValue, itemIndex) => this.setState({splittingMethod: itemValue})}>
-                    <Picker.Item label="Everyone pays his own share" value="own_share"/>
-                    <Picker.Item label="Divided evenly"              value="divided_even"/>
-                    <Picker.Item label="By way of a bill"            value="way_of_bill"/>
+                    {splitPicker}
                 </Picker>
                 <Button title='ADD' onPress={() => {
                     this.props.navigation.goBack()
                     // this.props.navigation.state.params.updateData(this.state.text)
                 }}/>
+            </View>
+        );
+    }
+}
+
+export default class ExpensePaidView extends React.Component
+{
+    static navigationOptions = ({navigation}) => ({
+        title: `Who paid?`,
+        headerLeft: null,
+    });
+
+    constructor(props)
+    {
+        super(props);
+        this.state = { };
+    }
+
+    render()
+    {
+        return (
+            <View>
+            </View>
+        );
+    }
+}
+
+export default class ExpenseOwedView extends React.Component
+{
+    static navigationOptions = ({navigation}) => ({
+        title: `Who owes what?`,
+        headerLeft: null,
+    });
+
+    constructor(props)
+    {
+        super(props);
+        this.state = { };
+    }
+
+    render()
+    {
+        return (
+            <View>
             </View>
         );
     }
