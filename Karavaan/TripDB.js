da28e5c53a0820c2dece11987f91fcd7c6ddd629
Tom Jacobs <Tom@192.168.0.17>
2017-11-28 12:30:10 +0100

    eerste poging voor asyncstoarge

--- Karavaan/TripDB.js
+++ Karavaan/TripDB.js
@@ -1,18 +1,32 @@
 import React from 'react';
 import {Trip} from './Trip';
+import {AsyncStorage} from 'react-native';
 
-export class TripDB extends React.Component
+export class TripDB
 {
+    static async createDB() // : Promise<TripDB>
+    {
+
+    }
+
     constructor()
     {
-        super();
+        this.trips = []
 
-        this.trips = [];
+        AsyncStorage.getItem('trips').then((x) => {
+            if (x.length !== 0)
+            {
+                this.trips = JSON.parse(x)
+            }
+        });
+        
     }
 
     addTrip(name, currencies){
         var trip = new Trip(name, currencies);
         this.trips.push(trip);
+
+        AsyncStorage.setItem('trips', JSON.stringify(this.trips)).then(() => {});
     }
 
     deleteTrip(name){
